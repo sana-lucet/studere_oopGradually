93a9cb7b939db68be1d268f06f8332bee718dd18
sana-lucet <sana.lucet@gmail.com>
2019-04-19 12:20:24 +0900

    [예광탄]중복된 숫자의 Ball을 뽑지 않도록 하는 알고리즘 추가

--- lotto/src/main/java/Main.java
+++ lotto/src/main/java/Main.java
@@ -3,13 +3,26 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("=====drawBox======");
         Box box = new Box();
         for (int i = 0; i < 6; i++){
             box.draw();
         }
+        int count = 0;
         for (Ball ball : box.balls){
-            System.out.println(ball);
+            count++;
+            System.out.println("counter:" + count + "\t" + ball);
         }
+//        System.out.println("\n=====ticketBox=====");
+//        Box ticket = new Box();
+//        for (int i = 0; i < 6; i++){
+//            ticket.draw();
+//        }
+//
+//        for (Ball ball : ticket.balls){
+//            System.out.println(ball);
+//        }
     }
 }
 
@@ -19,10 +32,22 @@ class Box{
     Random random = new Random();
 
     public void draw() {
-        balls[counter] = new Ball(random.nextInt(45) + 1);
-        counter++;
+        Ball ball;
+        if (!isDuplicate(ball = new Ball(random.nextInt(45) + 1))) {
+            balls[counter] = ball;
+            counter++;
+        }
     }
 
+    private boolean isDuplicate(Ball newBall) {
+        boolean result = false;
+        for (int i = 0; i < counter; i++){
+            if (balls[i].equals(newBall)){
+                result = true;
+            }
+        }
+        return result;
+    }
 }
 
 class Ball{
@@ -34,5 +59,13 @@ class Ball{
     @Override
     public String toString() {
         return "ball number: " + this.number;
+    }
+
+    public boolean equals(Object obj) {
+        Ball ball = (Ball) obj;
+        if (this.number == ball.number) {
+            return true;
+        }
+        return false;
     }
 }
